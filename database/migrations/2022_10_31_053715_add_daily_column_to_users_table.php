3c86708d30b3e8264ee672ef58597312622c419a
dimamuchalchuk <dimamuchalchuk@gmail.com>
2022-11-03 07:31:54 +0200

    small updates

--- database/migrations/2022_10_31_053715_add_daily_column_to_users_table.php
+++ database/migrations/2022_10_31_053715_add_daily_column_to_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->json('daily')->nullable()->after('day');
+            $table->json('daily')->nullable();
         });
     }
 
